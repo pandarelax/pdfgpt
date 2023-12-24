e8d80f8c03c7c6c468dc56925d0a9a88b93ed2b9
Efehan Turhan <94855256+pandarelax@users.noreply.github.com>
2023-12-25 00:59:42 +0300

    Delete empty space that used for test case

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 def main():
     st.set_page_config(page_title="PdfGPT App", layout="wide")
-    
     st.header("Chat With Your PDFs :books:")
     st.text_input("Ask a question about your PDFs")
     
