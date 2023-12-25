c64f6ebaba7f7ed5febf763109a285aea4617fca
Efehan Turhan <94855256+pandarelax@users.noreply.github.com>
2023-12-25 19:43:03 +0300

    Add get_pdf_text and get_text_chunks methods

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
+from ast import List
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
-def main():
+def get_pdf_text(pdfs) -> str:
+    text: str = ""
+    
+    for pdf in pdfs:
+        reader: PdfReader = PdfReader(pdf)
+        for page in reader.pages:
+            text+= page.extractText()
+    return text
+
+def get_text_chunks(text) -> List[str]:
+    splitter: CharacterTextSplitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks: List[str] = splitter.split_text(text)
+    return chunks
+    
+
+def main() -> None:
+    load_dotenv()
+    
     st.set_page_config(page_title="PdfGPT App", layout="wide")
     st.header("Chat With Your PDFs :books:")
     st.text_input("Ask a question about your PDFs")
+    
+    with st.sidebar:
+        st.subheader("Your Documents")
+        pdfs = st.file_uploader("Upload your PDFs", type=["pdf"], accept_multiple_files=True)
+        if pdfs:
+            with st.spinner("Processing PDFs..."):
+                # Get pdf text
+                raw_text = get_pdf_text(pdfs)
+
+                # Get the chunks from text
+                text_chunks = raw_text.split("\n\n")
+                
+                # Create vector store
+                st.write(pdfs)
+        
+        # st.file_uploader("Upload your PDFs", type=["pdf"])
+        # st.button("Process PDFs")
     
 if __name__ == "__main__":
     main()
